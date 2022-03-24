0796083bd951673bbb095de3d6c675e1b05d8484
João Estevan Barbosa <85952745+joaoestevanbarbosa@users.noreply.github.com>
2022-03-23 21:32:22 -0300

    Add files via upload

---
+++ 2 Filtering and Sorting Your Data/Applying+Criteria+With+WHERE+-+Example+Code.sql
@@ -0,0 +1,46 @@
+/*Applying criteria to a text field*/
+
+SELECT 
+	   [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+FROM [AdventureWorks2019].[HumanResources].[Employee]
+
+WHERE [JobTitle] = 'Research and Development Manager'
+
+
+/*Applying criteria to a numeric field*/
+
+SELECT 
+	   [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+FROM [AdventureWorks2019].[HumanResources].[Employee]
+
+WHERE [SalariedFlag] = 0
