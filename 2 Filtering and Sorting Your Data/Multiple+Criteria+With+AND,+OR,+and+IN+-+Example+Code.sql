c34e81b412cce03ab2d931ce49ecebe625fed32a
João Estevan Barbosa <85952745+joaoestevanbarbosa@users.noreply.github.com>
2022-04-04 20:34:49 -0300

    Add files via upload

---
+++ 2 Filtering and Sorting Your Data/Multiple+Criteria+With+AND,+OR,+and+IN+-+Example+Code.sql
@@ -0,0 +1,71 @@
+/*Example with AND*/
+
+SELECT [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[HumanResources].[Employee]
+
+  WHERE [JobTitle] = 'Sales Representative'
+  AND [MaritalStatus] = 'S'
+
+/*Example with OR*/
+
+
+  SELECT
+       [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[HumanResources].[Employee]
+
+  WHERE [JobTitle] = 'Sales Representative'
+  OR [MaritalStatus] = 'S'
+
+/*Example with IN*/
+
+
+  SELECT
+       [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[HumanResources].[Employee]
+
+  WHERE [JobTitle] IN('Senior Tool Designer', 'Research and Development Engineer', 'Senior Design Engineer')
