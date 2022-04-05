952f3f3caa1ef64f122288ec98fd09e792503aa2
João Estevan Barbosa <85952745+joaoestevanbarbosa@users.noreply.github.com>
2022-04-05 20:36:32 -0300

    Add files via upload

---
+++ 2 Filtering and Sorting Your Data/Matching+Text+Patterns+With+Wildcards+-+Example+Code.sql
@@ -0,0 +1,65 @@
+/*Using the % wildcard*/
+
+SELECT [BusinessEntityID]
+      ,[PersonType]
+      ,[NameStyle]
+      ,[Title]
+      ,[FirstName]
+      ,[MiddleName]
+      ,[LastName]
+      ,[Suffix]
+      ,[EmailPromotion]
+      ,[AdditionalContactInfo]
+      ,[Demographics]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[Person].[Person]
+
+  WHERE [FirstName] LIKE 'Tom%'
+  AND [LastName] LIKE 'B%'
+
+
+/*Pattern matching with brackets*/
+
+SELECT [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[HumanResources].[Employee]
+
+  WHERE [JobTitle] LIKE '%[0-9]%'
+
+
+/*Using NOT LIKE with brackets*/
+
+SELECT [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[HumanResources].[Employee]
+
+  WHERE [JobTitle] NOT LIKE '%[0-9]%'
