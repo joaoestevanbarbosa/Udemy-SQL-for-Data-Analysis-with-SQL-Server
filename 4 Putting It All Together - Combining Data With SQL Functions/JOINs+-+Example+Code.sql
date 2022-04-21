3b14086eb1bb1fbd78309b2957a2c3d50daedf7f
João Estevan Barbosa <85952745+joaoestevanbarbosa@users.noreply.github.com>
2022-04-20 21:25:07 -0300

    Add files via upload

---
+++ 4 Putting It All Together - Combining Data With SQL Functions/JOINs+-+Example+Code.sql
@@ -0,0 +1,16 @@
+/*Combining fields from different tables with JOIN*/
+
+SELECT 
+       B.FirstName
+      ,B.LastName
+      ,B.PersonType
+      ,A.[TerritoryID]
+      ,A.[SalesQuota]
+      ,A.[Bonus]
+      ,A.[CommissionPct]
+      ,A.[SalesYTD]
+      ,A.[SalesLastYear]
+
+  FROM [AdventureWorks2019].[Sales].[SalesPerson] A
+  JOIN [AdventureWorks2019].[Person].[Person] B
+  ON A.BusinessEntityID = B.BusinessEntityID
